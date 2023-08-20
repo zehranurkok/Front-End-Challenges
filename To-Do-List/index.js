dff07cf684ee580f4961135c02797f1f865b8760
zehranurkok <zehranurkok@gmail.com>
2023-08-20 22:19:40 +0300

    renew delete button

--- To-Do-List/index.js
+++ To-Do-List/index.js
@@ -1,5 +1,6 @@
 import express, { request } from "express";
 import bodyParser from "body-parser";
+import { localsName } from "ejs";
 
 const app = express();
 const port = 3000;
@@ -9,38 +10,60 @@ const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
 
 const d = new Date();
 let month = months[d.getMonth()];
-let day = days[d.getDay()-1];
+let day = days[ d.getDay() === 0 ? d.getDay()+6 : d.getDay()-1];
 let year = d.getFullYear();
 
 let taskList = [];
-let index = 0;
+let newTaskList = [];
+let index = -1;
+let newItem ="";
+let text = "";
 
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get("/", (req, res) => { 
-    res.render("app.ejs", { thisday: day, thismonth: month, thisyear: year});
+    index = 0;
+    taskList = [];
+    res.render("app.ejs", {thisday: day, thismonth: month, thisyear: year});
 })
 
 app.post("/submit", (req, res) => {
-    let text = req.body["addtask"];
+    text = req.body["addtask"];  
     if (text !== "" ) {
         taskList.push(text);
-        console.log(taskList.length);
-        res.render("app.ejs", {tasklist: taskList, i: index, task: text, thisday: day, thismonth: month, thisyear: year});
+    // console.log(text);
     }
-    app.get("/delete-all", (req, res) => { 
-        // taskList.splice(index,1);
-        taskList = [];
-        res.render("app.ejs", {tasklist: taskList, i: index, thisday: day, thismonth: month, thisyear: year});
-    })
+    res.render("app.ejs", {ntasklist: newTaskList, tasklist: taskList, i: index, task: text, thisday: day, thismonth: month, thisyear: year});
+
+})
+taskList.pop();
+
+
+
+app.get("/delete", (req, res) => { 
+    // taskIndex = taskList.indexOf(index);  
+    // console.log(newTaskList);
+    console.log(taskList);
+    console.log(text);
+    // let rTaskList = taskList.splice(index,1);
+    // taskList = taskList.filter(x => !rTaskList.includes(x));
+    
+    res.render("app.ejs", {ntasklist: newTaskList, tasklist: taskList, task: text, thisday: day, thismonth: month, thisyear: year});
+
 })
 
+app.get("/delete-all", (req, res) => { 
+    taskList = [];
+    res.render("app.ejs", {ntasklist:newTaskList, tasklist: taskList, i: index, task: text, thisday: day, thismonth: month, thisyear: year});
+})
 
 
 
 app.listen(port, () => {
     console.log(`Surver running on port ${port}.`);
 })
+
+
 
