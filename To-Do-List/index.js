0f90e3a2bff5711052a8bc655400bc67a47f7902
zehranurkok <zehranurkok@gmail.com>
2023-09-27 18:29:59 +0300

    feat: adding new item in list

--- To-Do-List/index.js
+++ To-Do-List/index.js
@@ -14,6 +14,8 @@ let month = months[d.getMonth()];
 let day = days[ d.getDay() === 0 ? d.getDay()+6 : d.getDay()-1];
 let year = d.getFullYear();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 db.connect('mongodb://127.0.0.1:27017/todolistDB');
 
@@ -23,49 +25,31 @@ const itemsSchema = new Schema ({
 
 const Item = db.model("Item", itemsSchema);
 
-let taskList = [];
-let newTaskList = [];
-let index = 0;
-let newItem ="";
-let text = "";
-
-app.use(express.static("public"));
-
-app.use(bodyParser.urlencoded({ extended: true}));
-
 app.get("/", (req, res) => { 
-    index = 0;
-    taskList = [];
-    const todoItems = Item.find({});
-    res.render("app.ejs", {thisday: day, thismonth: month, thisyear: year, newitems: todoItems});
+    let item = Item.find({}).then(item => {
+        res.render("app.ejs", { thisday: day, thismonth: month, thisyear: year, tasklist: item});
+        console.log(item); 
+    });
+    
 })
 
-// app.post("/delete", (req, res) => { 
-//     console.log(text);
-//     console.log(index);
-//     console.log(taskList); 
-//     let taskIndex = taskList.indexOf(text);  
-//     taskList = taskList.splice(taskIndex,1);
-//     console.log(taskIndex);
-//     res.render("app.ejs", {ntasklist: newTaskList, tasklist: taskList, i: index, task: text, thisday: day, thismonth: month, thisyear: year});
-// })
-
-app.post("/submit", (req, res) => {
-    text = req.body["addtask"];
+app.post("/", (req, res) => {
+    let text = req.body["addtask"];
     if (text !== "" ) {
-        // taskList[index] = text;
-        taskList.push(text);
-        // console.log(text);
+        const nItem = new Item ({
+            content: text
+        });
+        nItem.save();
+        res.redirect("/");
+    } else {
+        res.render("app.ejs", { task: text, thisday: day, thismonth: month, thisyear: year});
     }
-    // index++;
-    res.render("app.ejs", {ntasklist: newTaskList, tasklist: taskList, i: index, task: text, thisday: day, thismonth: month, thisyear: year});
 })
 
 app.post("/delete-all", (req, res) => { 
     taskList = [];
-    res.render("app.ejs", {ntasklist:newTaskList, tasklist: taskList, i: index, task: text, thisday: day, thismonth: month, thisyear: year});
+    res.render("app.ejs", { thisday: day, thismonth: month, thisyear: year});
 })
-
 
 app.listen(port, () => {
     console.log(`Surver running on port ${port}.`);
